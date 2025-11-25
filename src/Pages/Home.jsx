95a4ea2c9b4b510a0c66458a25b5649eb0bcd458
sameepabadhuge <sithusameepa@gmail.com>
2025-11-25 19:54:18 +0530

    update img for landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import homepageImg from '../assets/test2.avif';
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to home</h1>
+      
+      {/* image */}
+      <img
+          src={homepageImg}
+          alt="students learning"
+          className="w-1/4 md:w-1/2 mb-8 rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
+      />
     </div>
   )
 }
